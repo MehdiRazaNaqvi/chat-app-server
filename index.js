8cd5bd558d480c0ec0ed4a7f7902e59ae749eb99
MehdiRazaNaqvi <mehdiraza1232016@gmail.com>
2022-08-04 20:22:33 +0500

    initial

---
+++ index.js
@@ -0,0 +1,131 @@
+
+const express = require("express")
+const cors = require("cors")
+
+
+var bodyParser = require('body-parser');
+const e = require("express");
+
+
+
+const app = express()
+app.use(cors())
+
+
+
+app.use(express.json());
+
+app.use(express.urlencoded({ extended: false }))
+
+app.use(bodyParser.json());
+
+
+
+
+
+
+const port = process.env.PORT || 4000
+
+
+
+app.listen(port, () => {
+
+
+
+
+
+
+
+    const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+
+    const uri = "mongodb+srv://mehdi:mehdimongodb@cluster0.xuahs.mongodb.net/?retryWrites=true&w=majority";
+    const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+
+
+
+    app.get('/', (req, res) => {
+        res.send("Running")
+
+
+
+
+    })
+
+
+
+
+    app.get('/getpost', (req, res) => {
+
+        client.connect(err => {
+
+            //   const collection = client.db("database0").collection("col0");
+
+
+            client.db("database0").collection("post").find({}).toArray()
+                .then((ans) => res.send(ans))
+                .catch((err) => console.log(err))
+
+
+
+        })
+
+    })
+
+
+
+
+    app.post('/setpost', (req, res) => {
+
+
+
+
+
+        client.connect(err => {
+
+
+
+
+            client.db("database0").collection("post").insertOne(req.body)
+                .then((ans) => console.log("sent to database"))
+                .catch((err) => console.log(err))
+
+
+
+        })
+
+
+
+    })
+
+
+
+
+
+
+    app.post("/liked", (req, res) => {
+
+        // console.log(req.body)
+
+
+       
+
+
+        client.db("database0").collection("post").updateOne({ _id: ObjectId(req.body._id) }, { $push: { likers: req.body.uid } })
+        .then((r) => console.log("sucess"))
+
+    })
+
+
+
+
+
+
+
+
+
+
+
+
+})
